3a553df7990ea1b42f17ea9b20bda0ca6e4f4407
qingohng <qinghong1321@gmail.com>
2021-05-28 11:05:34 +0800

    First Commit

---
+++ Data/Posts.js
@@ -0,0 +1,13 @@
+import Posts_EN from "./Posts_EN";
+
+
+export default Posts = [
+    {
+        language: "English",
+        section: {
+            title: "Home",
+            body: Posts_EN
+        } 
+    },
+
+];
---
+++ Data/Posts_EN.js
@@ -0,0 +1,408 @@
+Posts_EN = [
+  {
+    type: "sections",
+    thumbnail: require("../assets/Test2.png"),
+    title: "Learning about Stroke",
+    //subtitle: "What is a stroke?",
+    body: [
+      {
+        type: "details",
+        thumbnail: require("../assets/Test2.png"),
+        title: "About Stroke",
+        subtitle: "What is a stroke?",
+        body: [
+          {
+            type: "h1",
+            content: "Analogy of a Stroke"
+          },
+          {
+            type: "img",
+            content: require("../assets/Test.png"),
+          },
+          {
+            type: "p",
+            content: "A stroke is a disease that affects the blood vessels, known as arteries, within the brain. It can be fatal to those who have had it, potentially causing various disabiliites or even death. "
+          },
+          {
+            type: "h1",
+            content: "How common is a Stroke?"
+          },
+          {
+            type: "img",
+            content: require("../assets/OneinSix.png"),
+          },
+        ]
+      },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      {
+        type: "details",
+        thumbnail: require("../assets/Test3.png"),
+        title: "Types of Strokes",
+        subtitle: "What are the 2 types of strokes?",
+        body: [
+          {
+            type: "h1",
+            content: "Ischaemic Stroke"
+          },
+          {
+            type: "img",
+            content: require("../assets/Test7.png"),
+          },
+          {
+            type: "p",
+            content: "Occurs when a blood clot is lodged in an artery and cuts off blood supply to the brain. Caused by either embolism or thrombosis. "
+          },
+          {
+            type: "img",
+            content: require("../assets/Blood.png"),
+          },
+
+          {
+            type: "h1",
+            content: "Haemorrhagic Stroke"
+          },
+          {
+            type: "img",
+            content: require("../assets/Rupture.png"),
+          },
+          {
+            type: "p",
+            content: "Occurs when there is a ruptured blood vessel causing bleeding in the brain. Where hemorrhage/blood leaks into the brain tissue. "
+          },
+          {
+            type: "p",
+            content: "It is usually caused by either high blood pressure or defects in the blood wall (cerebral aneurysm)."
+          },
+          {
+            type: "img",
+            content: require("../assets/cerebral2.png"),
+          },
+          {
+            type: "h1",
+            content: "Transient Ischaemic stroke"
+          },
+          {
+            type: "p",
+            content: "Occurs when there is a temporary interruption of blood supply to the brain. "
+          },
+          {
+            type: "p",
+            content: "There is no permanent brain damage and symptoms go away within a few minutes."
+          },
+        ]
+      },
+
+
+
+      
+      {
+        type: "details",
+        thumbnail: require("../assets/Test4.png"),
+        title: "About the Brain",
+        subtitle: "Different areas of the brain that can be affected by stroke.",
+        body: [
+          {
+            type: "h1",
+            content: "Brain Functions (Left)"
+          },
+          {
+            type: "img",
+            content: require("../assets/LeftBrain.jpg"),
+          },
+          {
+            type: "p",
+            content: "The brain has a left and right side. Each side is divided into several areas that control different function."
+          },
+
+        ]
+      },
+    ]
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // identifying a stroke
+  {
+    type: "sections",
+    thumbnail: require("../assets/Test9.png"),
+    title: "Identifying a Stroke",
+    //subtitle: "What is a stroke?",
+    body: [
+      {
+        type: "details",
+        thumbnail: require("../assets/Test9.png"),
+        title: "Signs & Symptoms",
+        subtitle: "Every patient is affected by a stroke differently, it depends on the area of damage in the brain.",
+        body: [
+          {
+            type: "h1",
+            content: "Physical"
+          },
+          {
+            type: "img",
+            content: require("../assets/Physical.png"),
+          },
+          {
+            type: "p",
+            content: "-	You may experience weakness in one or both sides of the body. This weakness varies in severity. "
+          },
+          {
+            type: "p",
+            content: "-	Weakness of the face muscles such as face drooping, drooling, and speech difficulty."
+          },
+          {
+            type: "p",
+            content: "-	You may have trouble standing, walking, or performing delicate tasks."
+          },
+          {
+            type: "h1",
+            content: "Sensation"
+          },
+          {
+            type: "img",
+            content: require("../assets/Sensation.png"),
+          },
+          {
+            type: "p",
+            content: "-	You may experience reduced or loss of sensation over one side or specific parts of your body."
+          },
+          {
+            type: "p",
+            content: "- You may experience abnormal extra feelings over one side of your body such as tingling or an electric sensation."
+          },
+
+          {
+            type: "h1",
+            content: "Vision"
+          },
+          {
+            type: "img",
+            content: require("../assets/Vision.png"),
+          },
+          {
+            type: "p",
+            content: "-	You may experience double-vision or loss of vision in one side or area of your vision field"
+          },
+
+          {
+            type: "h1",
+            content: "Communications"
+          },
+          {
+            type: "img",
+            content: require("../assets/Communication.png"), 
+          },
+          {
+            type: "p",
+            content: "-	Dysarthria: You may have trouble in articulating words. This may cause your speech to be unclear, slow or soft."
+          },
+          {
+            type: "p",
+            content: "-	Dysphasia: You may experience inability to understand or express words, phrases and sentences, in both verbal and written form."
+          },
+
+          {
+            type: "h1",
+            content: "Swallowing"
+          },
+          {
+            type: "img",
+            content: require("../assets/Swallowing.png"),
+          },
+          {
+            type: "p",
+            content: "- Dysphagia: You may have trouble with drinking and eating. It can cause coughing and choking leading to an increased risk of lung infection."
+          },
+        ]
+      },
+      {
+        type: "details",
+        thumbnail: require("../assets/Test11.png"),
+        title: "Thinking F.A.S.T",
+        subtitle: "How do you spot a Stroke?",
+        body: [
+          {
+            type: "h1",
+            content: "Identifying a Stroke"
+          },
+          {
+            type: "img",
+            content: require("../assets/Fast.png"),
+          },
+          {
+            type: "p",
+            content: "To identify if a person is having a Stroke, think F.A.S.T"
+          },
+        ]
+      },
+    ]
+  },
+  // },
+  // ]
+  // },
+  //   { type: "details",
+  //     thumbnail: require("../assets/Test11.png"),   
+  //     title: "Thinking F.A.S.T",
+  //     subtitle: "How do you spot a Stroke?",
+  //     body: [
+  //       {
+  //         type: "h1",
+  //         content: "Identifying a Stroke" 
+  //       },
+  //       {
+  //         type: "img",
+  //         content: require("../assets/Fast.png"),
+  //       },
+  //       {
+  //         type: "p",
+  //         content: "To identify if a person is having a Stroke, think F.A.S.T"
+  //       },
+  //     ]
+  //   },
+  //   ]   
+  // },
+  // Managing the Risk
+
+
+
+
+{
+  type: "sections",
+  thumbnail: require("../assets/rehab_thumbnail.jpeg"),
+  title: "Rehabilitation and Exercises",
+  body: [
+{
+  thumbnail: require("../assets/rehab_thumbnail.jpeg"),
+  title: "Ankle Movement",
+  subtitle: "10 repetitions",
+  body: [
+    {
+      type: "h1",
+      content: "Step 1",
+    },
+    {
+      type: "p",
+      content: "Sit down on a chair and raise one leg.",
+    },
+    {
+      type: "img",
+      content: require("../assets/AM_1.jpeg"),
+    },
+    {
+      type: "h1",
+      content: "Step 2",
+    },
+    {
+      type: "p",
+      content: "Point your toes towards yourself.",
+    },
+    {
+      type: "img",
+      content: require("../assets/AM_gif.gif"),
+    },
+  ]
+},
+
+
+{
+  thumbnail: require("../assets/rehab_thumbnail.jpeg"),
+  title: "One Leg Stand",
+  subtitle: "10 seconds on each leg",
+  body: [
+    {
+      type: "h1",
+      content: "Step 1",
+    },
+    {
+      type: "p",
+      content: "Stand up straight with support.",
+    },
+    {
+      type: "img",
+      content: require("../assets/OLS_1.jpeg"),
+    },
+    {
+      type: "h1",
+      content: "Step 2",
+    },
+    {
+      type: "p",
+      content: "Raise up one leg and hold for 10 seconds before switching to the other leg.",
+    },
+    {
+      type: "img",
+      content: require("../assets/OLS_gif.gif"),
+    },
+
+  ]
+},
+
+
+{
+  thumbnail: require("../assets/rehab_thumbnail.jpeg"),
+  title: "Grip n Release",
+  subtitle: "10 repetitions",
+  body: [
+    {
+      type: "h1",
+      content: "Step 1",
+    },
+    {
+      type: "p",
+      content: "Rest arm on table with palm facing upwards",
+    },
+    {
+      type: "img",
+      content: require("../assets/GaR_1.jpeg"),
+    },
+    {
+      type: "h1",
+      content: "Step 2",
+    },
+    {
+      type: "p",
+      content: "Place a pen in hand.",
+    },
+    {
+      type: "img",
+      content: require("../assets/GaR_2.jpeg"),
+    },
+    {
+      type: "h1",
+      content: "Step 3",
+    },
+
+    
+  ]
+},
+
+  ]
+},
+]
+
+export default Posts_EN 
